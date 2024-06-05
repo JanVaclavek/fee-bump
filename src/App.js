b3b23ffd6ac707ccc903b84dd0a06f23e88af447
JanVaclavek <61776178+JanVaclavek@users.noreply.github.com>
2024-06-05 17:56:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import { useState } from "react";
+import Select from "react-select";
 
 function App() {
+  const calculateFeerate = () => {
+    const sizes = [178, 246, 314, 382, 450, 518, 586, 654, 722, 790];
+    // console.log(targetedFeeRate, feeRateNeeded, transactionNumber);
+    const txSize = sizes[transactionNumber - 1];
+    const feeRate =
+      (targetedFeeRate * (197 * transactionNumber + txSize) -
+        transactionNumber) /
+      txSize;
+    setFeeRateNeeded(feeRate);
+  };
+
+  const options = [
+    { value: 1, label: 1 },
+    { value: 2, label: 2 },
+    { value: 3, label: 3 },
+    { value: 4, label: 4 },
+    { value: 5, label: 5 },
+    { value: 6, label: 6 },
+    { value: 7, label: 7 },
+    { value: 8, label: 8 },
+    { value: 9, label: 9 },
+    { value: 10, label: 10 },
+  ];
+
+  const [targetedFeeRate, setTargetedFeerate] = useState(1);
+  const [feeRateNeeded, setFeeRateNeeded] = useState(1);
+  const [transactionNumber, setTransactionNumber] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div style={{width: '480px', margin: '0 auto'}}>
+        <div>
+          {"Targeted feerate:"}{" "}
+          <input
+            type="text"
+            onChange={(event) => setTargetedFeerate(event.target.value)}
+          ></input>
+        </div>
+        <div>
+          <Select
+            options={options}
+            onChange={(number) => setTransactionNumber(number.value)}
+          />
+        </div>
+        <div>
+          <button onClick={() => calculateFeerate()}>Get feerate</button>
+        </div>
+        {feeRateNeeded != 0 && feeRateNeeded}
+      </div>
     </div>
   );
 }
