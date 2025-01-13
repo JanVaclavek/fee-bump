a472e237e7aeffeae330f3b2af6bdb37683cd81d
JanVaclavek <61776178+JanVaclavek@users.noreply.github.com>
2025-01-13 12:06:42 +0100

    Extend number of bumped transactions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Select from "react-select";
 
 function App() {
   const calculateFeerate = () => {
-    const sizes = [178, 246, 314, 382, 450, 518, 586, 654, 722, 790];
+    console.log("aaa");
+    // const sizes = [178, 246, 314, 382, 450, 518, 586, 654, 722, 790];
     // console.log(targetedFeeRate, feeRateNeeded, transactionNumber);
-    const txSize = sizes[transactionNumber - 1];
-    const feeRate =
+    // const txSize = sizes[transactionNumber - 1];
+    const txSize = 110 + transactionNumber * 68;
+    const feeRate = 
       (targetedFeeRate * (197 * transactionNumber + txSize) -
-        transactionNumber) /
+        197* transactionNumber ) /
       txSize;
     setFeeRateNeeded(feeRate);
   };
@@ -25,6 +27,26 @@ function App() {
     { value: 8, label: 8 },
     { value: 9, label: 9 },
     { value: 10, label: 10 },
+    { value: 11, label: 11 },
+    { value: 12, label: 12 },
+    { value: 13, label: 13 },
+    { value: 14, label: 14 },
+    { value: 15, label: 15 },
+    { value: 16, label: 16 },
+    { value: 17, label: 17 },
+    { value: 18, label: 18 },
+    { value: 19, label: 19 },
+    { value: 20, label: 20 },
+    { value: 1, label: 21 },
+    { value: 22, label: 22 },
+    { value: 23, label: 23 },
+    { value: 24, label: 24 },
+    { value: 25, label: 25 },
+    { value: 26, label: 26 },
+    { value: 27, label: 27 },
+    { value: 28, label: 28 },
+    { value: 29, label: 29 },
+    { value: 30, label: 30 },
   ];
 
   const [targetedFeeRate, setTargetedFeerate] = useState(1);
@@ -55,5 +77,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
